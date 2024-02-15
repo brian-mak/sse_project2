ba5c5e1561a1aee195dcc71329bce8d209fe05aa
Brian Mak <bm323@imperial.ac.uk>
2024-02-15 14:30:06 +0000

    add database.py

---
+++ api/database.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+from os import environ as env
+import pyodbc, struct
+from azure import identity
+from dotenv import find_dotenv, load_dotenv
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+connection_string = env.get("AZURE_SQL_CONNECTIONSTRING")
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def root():
+    print("Root of User API")
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+
+        print("connected")
+        # Table should be created ahead of time in production app.
+        cursor.execute("""
+            CREATE TABLE Users (
+                Email varchar(255) NOT NULL PRIMARY KEY IDENTITY,
+                Name varchar(255),
+                NickName varchar(255),
+                LastLogin datetime
+            );
+        """)
+
+        conn.commit()
+    except Exception as e:
+        # Table may already exist
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+    return "User API"
+
+def get_conn():
+    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
+    conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+    return conn
+
+if __name__ == "__main__":
+    root()
