bb225a3130854a1dfd48c789da79a34589490df9
Brian Mak <bm323@imperial.ac.uk>
2024-02-21 15:44:19 +0000

    find_partner docker

--- api/find_partner.py
+++ api/find_partner.py
@@ -3,20 +3,15 @@ import sys, os
 import database
 from datetime import datetime
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    workout_invitations = database.get_all_user()
-    return jsonify(workout_invitations)
+api_url = os.getenv('BOOK_API_URL', 'http://book-api-server-dns.cye8ahgvh7b6dkea.uksouth.azurecontainer.io:5000/books')
 
 
-@app.route('/post_invitation', methods=['POST'])
+@app.route('/post_invitation', methods=['GET'])
 def post_invitation():
     input = request.form
-    print(input)
     try:
-        new_invitation = {
+        query_params = {
             'user_id': session["user"]["userinfo"]["sub"],
             'workout_id': 1, #input.get("workout_type"),
             'location': input.get("location"),
@@ -24,48 +19,11 @@ def post_invitation():
             'end_time': datetime.strptime(input.get("end_time"), '%Y-%m-%dT%H:%M'),
             'message': input.get("message")
         }
-        database.post_invitation(new_invitation)
-        return ('Workout Invitation Posted')
+        response = requests.get(api_url, params=query_params)
+        if response['success'] == false:
+            return (f"Post workout invitation failed. Error: {response['message']}")    
+        return response
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         return (f"Post workout invitation failed. Error: {exc_type} {fname} {exc_tb.tb_lineno}")
-    
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    query_params = request.args
-    try:
-        # new_message = {
-        #     'user_id': session["user"]["userinfo"]["sub"],
-        #     'post_id': post_id,
-        #     'receiver_id': receiver_id,
-        #     'message': ("message")
-        # }
-        database.post_new_message(query_params)
-        return ('Message Sent')
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-        return (f"Send message failed. Error: {exc_type} {fname} {exc_tb.tb_lineno}")
-
-
-@app.route('/get_post', methods=['POST'])
-def get_post():
-    query_params = request.args
-    try:
-        user_id = query_params.get('user_id')
-        if user_id == None:
-            return ('Invalid Input: User_ID empty')
-        posts = database.get_posts(user_id)
-        return (jsonify(posts))
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-        return (f"Send message failed. Error: {exc_type} {fname} {exc_tb.tb_lineno}")
-    
-
-if __name__ == '__main__':
-    app.run()
---
+++ api/find_partner_api/database.py
@@ -0,0 +1,169 @@
+from flask import Flask, jsonify, request, Blueprint
+from os import environ as env
+import os
+import pyodbc
+from dotenv import find_dotenv, load_dotenv
+import sys
+from retry import retry
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+connection_string = env.get("AZURE_SQL_CONNECTIONSTRING")
+
+
+@retry(Exception, tries=3, delay=1, backoff=2)
+def get_conn():
+    conn = pyodbc.connect(connection_string)
+    return conn
+
+
+def create_partner_post():
+    print("Create Partner Post")
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+
+        print("connected")
+        # Table should be created ahead of time in production app.
+        cursor.execute("""
+            CREATE TABLE workout_posts (
+                id int NOT NULL PRIMARY KEY IDENTITY,
+                user_id varchar(255) NOT NULL FOREIGN KEY REFERENCES Users(User_ID),
+                workout_id int NOT NULL FOREIGN KEY REFERENCES Workouts(WorkoutID),
+                location varchar(255) NOT NULL,
+                start_time datetime,
+                end_time datetime,
+                post_time datetime,
+                message text
+            );
+        """)
+
+        conn.commit()
+    except Exception as e:
+        # Table may already exist
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
+    return 
+
+
+def create_message_log():
+    print("Create Message Log")
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+
+        print("connected")
+        # Table should be created ahead of time in production app.
+        cursor.execute("""
+            CREATE TABLE message_log (
+                id int NOT NULL PRIMARY KEY IDENTITY,
+                post_id int NOT NULL FOREIGN KEY REFERENCES workout_posts(id),
+                user_id varchar(255) NOT NULL FOREIGN KEY REFERENCES Users(ID),
+                sent_time datetime,
+                message text
+            );
+        """)
+
+        conn.commit()
+    except Exception as e:
+        # Table may already exist
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
+    return 
+
+
+def get_all_invitations():
+    posts = []
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        print("connected")
+        # Table should be created ahead of time in production app.
+        cursor.execute("""SELECT * FROM workout_posts""")
+        posts = cursor.fetchall()
+    except Exception as e:
+        # Table may already exist
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
+    return posts
+
+
+def post_invitation(new_invitation):
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        print("connected")
+        cursor.execute("""
+                INSERT INTO message_log (user_id, workout_id, location, start_time, end_time, post_time, message) VALUES (?, ?, ?, ?, ?, GETDATE(), ?)
+            """, (new_invitation['user_id'], new_invitation['workout_id'], new_invitation['location'], 
+                  new_invitation['start_time'].strftime('%Y-%m-%d %H:%M:%S'), new_invitation['end_time'].strftime('%Y-%m-%d %H:%M:%S'), new_invitation['message']))
+        conn.commit()
+        return jsonify({"success": True, "message": "Post added successfully."})
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        return jsonify({"success": False, "message": str(e)})
+
+
+def post_new_message(new_message):
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        print("connected")
+        cursor.execute("""
+                INSERT INTO workout_posts (user_id, post_id, receiver_id, sent_time, message) VALUES (?, ?, ?, GETDATE(), ?)
+            """, (new_message['user_id'], new_message['post_id'], new_message['receiver_id'], new_message['message']))
+        conn.commit()
+        print ("new message added")
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
+    return
+
+
+def get_posts(user):
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        print("connected")
+        cursor.execute("""
+                SELECT * FROM workout_posts WHERE user_id = ?
+            """, (user))
+        posts = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        data = []
+        for row in posts:
+            data.append(dict(zip(columns, row)))
+        return jsonify({"success": True, "data": data})
+        
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        return jsonify({"success": False, "message": str(e)})
+
+
+def get_message(user):
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        print("connected")
+        cursor.execute("""
+                SELECT * FROM message_log WHERE user_id = ? OR receiver_id = ?
+            """, (user, user))
+        messages = cursor.fetchall()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
+    return messages
