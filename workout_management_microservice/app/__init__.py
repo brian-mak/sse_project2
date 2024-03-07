648ce61f351e0fb875081a272464f7edc33d91b0
brandonloke <brandon.loke23@imperial.ac.uk>
2024-03-07 17:19:22 +0000

    BL fix microservice and containerised

--- workout_management_microservice/app/__init__.py
+++ workout_management_microservice/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from .models import db
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config.Config')
 
     db.init_app(app)
--- workout_management_microservice/config.py
+++ workout_management_microservice/config.py
@@ -3,7 +3,5 @@ import urllib
 
 class Config:
     params = urllib.parse.quote_plus(os.getenv('AZURE_SQL_CONNECTIONSTRING'))
-    print(params)
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
